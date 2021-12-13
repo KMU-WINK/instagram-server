6afc66758d9e05dea98057f8df0d4a12bd501e0b
ChanHHOO <hpyho33@kookmin.ac.kr>
2021-12-13 20:04:23 +0900

    commend and article fk et

---
+++ migrations/20211213100913-fk-commend.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+
+    await queryInterface.addColumn("comments", "article_id", {
+      type: Sequelize.INTEGER,
+    });
+    await queryInterface.addConstraint("comments", {
+      fields: ["article_id"],
+      type: "foreign key",
+      name: "articles_comments_id_fk",
+      references: {
+        table: "articles",
+        field: "id",
+      },
+      onDelete: "cascade",
+      onUpdate: "cascade",
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+     await queryInterface.removeColumn("comments", "post_id");
+  }
+};
