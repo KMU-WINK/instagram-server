4f355ac1e2f9824eccd7d5659d3b8b2ff4cc575d
sirldev <sirl0@naver.com>
2021-12-18 23:20:21 +0900

    feat: Swagger UI 추가

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
+
+const { swaggerUi, specs } = require('./modules/swagger');
+
 const app = express();
 const port = 3000;
+
+// API Server Docs
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 app.get("/", (req, res) => {
 	res.send("Hello World!");
---
+++ modules/swagger.js
@@ -0,0 +1,26 @@
+const swaggerUi = require("swagger-ui-express");
+const swaggereJsdoc = require("swagger-jsdoc");
+
+const options = {
+	swaggerDefinition: {
+		info: {
+			title: "[WINK x STACK] Instagram API",
+			version: "1.0.0",
+			description: "Instagram API Server Docs",
+		},
+		host: "localhost:3000",
+		basePath: "/",
+	},
+	apis: [
+		// Update TODO
+		// "./routes/*.js",
+		"./swagger/*",
+	],
+};
+
+const specs = swaggereJsdoc(options);
+
+module.exports = {
+	swaggerUi,
+	specs,
+};
